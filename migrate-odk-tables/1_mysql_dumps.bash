d9bbcf5a15dd5450949efcc4047eb29d9e00e7d5
Brent Atkinson <brent.atkinson@gmail.com>
2018-02-02 09:27:04 +0100

    Create named mysql database before attempting to import

--- migrate-odk-tables/1_mysql_dumps.bash
+++ migrate-odk-tables/1_mysql_dumps.bash
@@ -4,6 +4,7 @@ read -p "MySQL Host: " DB_HOST
 read -p "MySQL Port: " DB_PORT
 read -p "MySQL User: " DB_USER
 read -s -p "MySQL Password ($DB_USER): " DB_PASS
+echo
 
 for db in odk_attic odk_prod
 do
